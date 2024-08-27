93c87cb13e6cf561ee5a1abc04444b63ef8f8f77
Hamza Akıllı <163044736+Hamzaakl@users.noreply.github.com>
2024-08-27 13:03:29 +0300

    Port Scan
    Python Kullanarak Basit Port Tarayici

---
+++ Port_Scan.py
@@ -0,0 +1,18 @@
+import socket
+
+
+ip = '10.10.10.101'
+
+for port in range(1,100):
+    
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip,port))
+        print(str(port),": open")
+    
+    except Exception as e:
+        print(str(port),"closed")        
+        pass   
+    finally:
+        s.close()
+    
